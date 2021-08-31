6f864de17561b23c841c617ff801dd26274e647e
igor.kankov <igor.kankov@kindgeek.com>
2021-08-31 15:47:55 +0300

    task 3 redone to filter method

--- home-task-3/task3.js
+++ home-task-3/task3.js
@@ -1,7 +1,11 @@
 const array = [1, 3, 4, 2, 3, 1, 4, 5];
 
-const uniqArray = (arr) => {
-  return [...new Set(arr)].sort((a, b) => a - b)
+// const uniqArray = (arr) => {
+//   return [...new Set(arr)].sort((a, b) => a - b)
+// };
+
+const uniqArray = (array) => {
+  return array.filter((el, ind, arr) => arr.indexOf(el) === ind).sort((a, b) => a - b)
 };
 
 console.log(uniqArray(array));
